af0909a391d91cc4d5e2fad9d1cb0a8c6ad84ac0
BahadirDogrusoz <dogrusozba@gmail.com>
2020-09-24 14:10:19 +0300

    Strings

---
+++ main.py
@@ -0,0 +1,13 @@
+isim = "Bahadır"
+print(isim)
+
+name = "Mustafa"
+surname = "Doğrusöz"
+
+print(name + " " + surname)
+
+metin = "Hello Türkiye"
+print(metin[2:7])
+
+print(metin.lower())
+print(metin.upper())
